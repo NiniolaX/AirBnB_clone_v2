4c2662365ff684c15179916ce595a72c546d3f00
NiniolaX <loveafinni16@gmail.com>
2024-02-24 20:26:22 +0100

    Cleaned up syntax errors

--- models/base_model.py
+++ models/base_model.py
@@ -10,14 +10,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-Base = declarative_base()
-
 
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True)
-    created_at = Column(Datetime(), default=datetime.utcnow(), nullable=False)
-    updated_at = Column(Datetime(), default=datetime.utcnow(), nullable=False)
+    created_at = Column(Datetime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(Datetime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
