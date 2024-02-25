05ac31bb47772e04f5d8e32e469864c4be63bf3b
Dev Manuels <Emmanuel.umeile@gmail.com>
2024-02-25 05:49:26 +0100

    ensure pycodestyle compliance

--- models/city.py
+++ models/city.py
@@ -9,7 +9,7 @@ class City(BaseModel, Base):
     """ The city class for modelling city objects """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column( String(60), ForeignKey("states.id"), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     places = relationship(
         "Place",
         backref="user",
