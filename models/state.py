d696a2ff870e68eb95483d0a2c3640b2869237ab
NiniolaX <loveafinni16@gmail.com>
2024-02-27 12:41:32 +0100

    Fixed logical error with relationship setup between states and cities

--- models/state.py
+++ models/state.py
@@ -11,7 +11,6 @@ class State(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'states'
         name = Column(String(128), nullable=False, default='')
-        from models import City
         cities = relationship('City', backref='state',
                               cascade='all, delete-orphan')
     else:
