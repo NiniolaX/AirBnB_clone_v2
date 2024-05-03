65cc5e9862c2697d24069173b9819a489dfdca54
NiniolaX <loveafinni16@gmail.com>
2024-05-03 12:16:24 +0100

    Fixed destroy command

--- console.py
+++ console.py
@@ -221,7 +221,8 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            obj = storage.all()[key]
+            storage.delete(obj)
             storage.save()
         except KeyError:
             print("** no instance found **")
