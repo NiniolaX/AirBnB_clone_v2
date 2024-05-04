b189bc0e09706f8f75d8162929bad56e0f523f9a
NiniolaX <loveafinni16@gmail.com>
2024-05-04 19:15:09 +0100

    Wrote script that compreseses web static directory for deployment

---
+++ 1-pack_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Script generates a .tgz archive from the contents of the web_static folder
+
+Functions:
+    do_pack: Generates .tgz archives of a specified folder
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+@task
+def do_pack():
+    """ Generates a .tgz archive from a specified folder"""
+    # Get current datetime
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+
+    source = "web_static"
+    destination = "versions"
+
+    # Create destination folder if it doesn't exist
+    local(f"if [ ! -d {destination} ]; then mkdir {destination}; fi")
+    archive_name = f"{destination}/{source}_{timestamp}.tgz"
+
+    # Run command
+    result = local(f"tar -cvzf {archive_name} {source_file}")
+    if result.succeeded:
+        return archive_name
+    else:
+        return None
+
+
