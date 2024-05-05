41be00ec421daf4d872cecd13f1f0dbd196971ab
Love Afinni <loveafinni16@gmail.com>
2024-05-05 01:13:41 -0700

    Fixed bugs in script: Script was throwing a "Permission denied" error because env.user did not have access to write destination folder Gave env.user write permissions

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,12 +4,12 @@ This module contains a Fabric function that distributes an archive to my
 web servers.
 Archive is web_static archive created with 1-pack_web_static.py script
 """
-from fabric.api import run, env, put
+from fabric.api import run, env, put, sudo
 import os
 
 # Specify host information
 env.user = 'ubuntu'
-env.hosts = ['34.229.184.178', '54.83.138.88']
+env.hosts = ['34.229.184.178']
 env.key_filename = '~/.ssh/servers'
 
 
@@ -20,31 +20,37 @@ def do_deploy(archive_path):
     Return:
         (bool): True if all operations have been done correctly, else False.
     """
+    # If file at path archive_path does not exist, return False
     if not os.path.exists(archive_path):
-        # If file at path archive_path does not exist, return False
+        print('Path does not exist')
         return False
 
-    # Define relevant variables
-    archive_name = archive_path.split('/')[-1].split('.')[0]
-    destination_folder = "/data/web_static/releases"
-    path_to_decomp_archive = f'{destination_folder}/{archive_name}/'
-    symlink_to_curr_release = "/data/web_static/current"
+    # Upload the archive to /tmp/ directory of web servers
+    remote_archive_path = put(archive_path, '/tmp/')[0]
 
-    # Upload the archive to /tmp directory of web servers
-    put(archive_path, '/tmp/')
+    # Define relevant variables
+    archive_name = remote_archive_path.split('/')[-1].split('.')[0]
+    destination_folder = "/data/web_static/releases"
+    path_to_decomp_archive = f'{destination_folder}/{archive_name}'
+    symlink_to_curr_release = "/data/web_static/current"
 
     # Uncompress archive to destination folder on web server
     run(f'sudo mkdir -p {path_to_decomp_archive}')
-    run(f'tar -xzf /tmp/{archive_name} -C {path_to_decomp_archive}')
+    run(f'sudo chown -R ubuntu:ubuntu {path_to_decomp_archive}')
+    run(f'tar -xzf {remote_archive_path} -C {path_to_decomp_archive}')
+
+    # Move decompressed files one level up
+    #run(f'mv {path_to_decomp_archive}/web_static {path_to_decomp_archive}')
+    #run(f'rm -rf {path_to_decomp_archive}/web_static')
 
     # Remove archive from web server
-    run(f'sudo rm /tmp/{archive_name}.tgz')
+    run(f'sudo rm {remote_archive_path}')
 
     # Delete symbolic link $symlink from server
     run(f'sudo rm {symlink_to_curr_release}')
 
     # Create new symbolic link to new version of code
-    run(f'ln -s {path_to_decomp_archive} {symlink_to_curr_release}')
+    run(f'sudo ln -s {path_to_decomp_archive} {symlink_to_curr_release}')
 
     # Restart nginx
     run('sudo service nginx restart')
