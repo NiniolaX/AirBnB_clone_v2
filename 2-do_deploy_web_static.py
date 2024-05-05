388c8ed7f09192d4e62bd34eb7720a4f1fee4956
Love Afinni <loveafinni16@gmail.com>
2024-05-05 09:39:26 -0700

    Rewrote script

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,63 +1,60 @@
 #!/usr/bin/python3
 """
-This module contains a Fabric function that distributes an archive to my
-web servers.
-Archive is web_static archive created with 1-pack_web_static.py script
+This Fabric script distributes compressed static code files (archive) to
+some web servers.
 """
-from fabric.api import run, env, put, sudo
+from fabric.api import put, run, env
 import os
 
-# Specify host information
 env.user = 'ubuntu'
 env.hosts = ['34.229.184.178', '54.83.138.88']
 env.key_filename = '~/.ssh/servers'
 
 
 def do_deploy(archive_path):
-    """ Distributes an archive to some web servers
+    """Distributes an archive to web servers.
     Args:
-        archive_path: Path to archive to be distributed on servers
+        archive_path(str): Local path to archive
     Return:
-        (bool): True if all operations have been done correctly, else False.
+        (bool): True on success, otherwise, False
     """
-    # If file at path archive_path does not exist, return False
-    if not os.path.exists(archive_path):
-        print(f'Path {archive_path} does not exist')
+    try:
+        # Check if passed archive path exists
+        if not os.path.exists(archive_path):
+            print(f'File {archive_path} not found')
+            return False
+
+        # Extract necessary paths
+        archive_name = os.path.basename(archive_path)
+        tmp_folder = '/tmp/'
+        dest_folder = '/data/web_static/releases/'
+        release_symlink = '/data/web_static/current'
+        uncomp_folder_path = f'{dest_folder}/{archive_name}'.split('.')[0]
+
+        # Upload archive file to temporary folder (/tmp/)
+        put(archive_path, tmp_folder)
+
+        # Create destination folder if it doesn't exist
+        run(f'if [ ! -d {dest_folder} ]; then sudo mkdir {dest_folder}; fi')
+
+        # Uncompress archive in temporary folder to destination folder
+        run(f'tar -xzf {tmp_folder}/{archive_name} -C {dest_folder}')
+        # Rename uncompressed folder
+        run(f'mv {dest_folder}/web_static {uncomp_folder_path}')
+
+        # Delete archive from web server
+        run(f'rm -rf {tmp_folder}/{archive_name}')
+
+        # Update symbolic link to current release
+        run(f'sudo rm -rf {release_symlink}')
+        run(f'sudo ln -s {uncomp_folder_path}/ {release_symlink}')
+
+        # Restart server
+        run('sudo service nginx restart')
+
+        print('New version deployed!')
+        return True
+
+    except Exception as e:
+        print(f'Error during deployment: {e}')
         return False
-
-    print('Deploying new version...')
-
-    # Upload the archive to /tmp/ directory of web servers
-    remote_archive_path = put(archive_path, '/tmp/')[0]
-
-    # Define relevant variables
-    archive_name = remote_archive_path.split('/')[-1].split('.')[0]
-    dest_folder = "/data/web_static/releases"
-    path_to_decomp_archive = f'{dest_folder}/{archive_name}'
-    symlink_to_curr_release = "/data/web_static/current"
-
-    # Create dest_folder if it doesn't exist
-    run(f'if [ -d {dest_folder} ]; then mkdir -p {dest_folder}; fi')
-
-    # Uncompress archive to destination folder on web server
-    run(f'tar -xzf {remote_archive_path} -C {dest_folder}')
-
-    # Rename decompressed folder with respect to version name
-    run(f'mv {dest_folder}/web_static {path_to_decomp_archive}')
-    # run(f'rm -rf {path_to_decomp_archive}/web_static')
-
-    # Remove archive from web server
-    run(f'sudo rm {remote_archive_path}')
-
-    # Delete symbolic link $symlink from server
-    run(f'sudo rm {symlink_to_curr_release}')
-
-    # Create new symbolic link to new version of code
-    run(f'sudo ln -s {path_to_decomp_archive}/ {symlink_to_curr_release}')
-
-    # Restart nginx
-    run('sudo service nginx restart')
-
-    # Return True if all operations exited successfully
-    print('New version deployed!')
-    return True
