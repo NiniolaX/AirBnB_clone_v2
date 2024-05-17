41fc5f48a88cff0a17dc91571b19192911d93ee1
NiniolaX <loveafinni16@gmail.com>
2024-05-17 16:04:39 +0100

    Script starts a new Flask web application: Added new route /c/<text>

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,9 +3,10 @@
 Routes:
     /: display “Hello HBNB!”
     /hbnb: display “HBNB”
-    /
+    /c/<text>: display “C ” followed by the value of the text variable
 """
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -22,5 +23,11 @@ def hbnb_route():
     return "HBNB"
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ Displays 'C' followed by the value of the text variable """
+    return f'C {escape(text)}'.replace('_', ' ')
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
