0e407ec1db2ddd8e0640c60e5742e06fd4cf7021
NiniolaX <loveafinni16@gmail.com>
2024-05-19 19:27:27 +0100

    Modified template format to meet requirements

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,8 +16,9 @@ def states_list():
     """ Returns a template listing all states in `DBStorage` """
     from models.state import State
     storage.reload()
-    states = list(storage.all(State).values())
-    return render_template('7-states_list.html', states=states)
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
