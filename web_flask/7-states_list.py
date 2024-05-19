1d38b66849838e55fbd23b72f31b8264f8df8f34
NiniolaX <loveafinni16@gmail.com>
2024-05-19 19:38:53 +0100

    Removed call to reload

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,9 +13,8 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ Returns a template listing all states in `DBStorage` """
+    """ Returns a template displaying all states """
     from models.state import State
-    storage.reload()
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda x: x.name)
     return render_template('7-states_list.html', states=sorted_states)
