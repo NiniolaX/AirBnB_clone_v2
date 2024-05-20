7a0e194c4753d0c3f70a29572eb21de7d4e513a7
NiniolaX <loveafinni16@gmail.com>
2024-05-20 06:54:55 +0100

    Added script that starts a web application

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -17,12 +17,29 @@ app = Flask(__name__)
 # app.jinja_env.lstrip_blocks = True
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
+@app.route('/states', strict_slashes=False)
+def state():
     """ Returns a template displaying all states """
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda x: x.name)
     return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def cities_in_states(id):
+    """ Returns a template displaying the cities in a state
+    Args:
+        (str): id of state whose cities are to be displayed
+    """
+    states = storage.all(State)
+    state_id = f'State.{id}'
+    if state_id in states:
+        state = states.get(state_id)
+        sorted_cities = sorted(state.cities, key=lambda x: x.name)
+    else:
+        state = None
+        sorted_cities = None
+    return render_template('9-states.html', state=state, cities=sorted_cities)
 
 
 @app.teardown_appcontext
