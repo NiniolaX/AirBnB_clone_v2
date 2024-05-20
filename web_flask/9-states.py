7268c0d24dbdfcf002bfcf9e8432f4663d45d134
NiniolaX <loveafinni16@gmail.com>
2024-05-20 05:41:43 +0100

    Made all scripts executable

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Script starts a web flask application listening on 0.0.0.0 port 5000
 Routes:
-    /cities_by_states: Displays a HTML page with the list of all city objects
-        by state
+    /states_list: Displays a HTML page with the list of all State objects
+        sorted by name (A-Z)
 """
 from flask import Flask, render_template
 from models import storage
@@ -12,17 +12,17 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_state():
-    """ Returns a template displaying all cities by their states """
+# Configure Jinja2 environment
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Returns a template displaying all states """
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda x: x.name)
-    cities_dict = {}
-    for state in states:
-        sorted_cities = sorted(state.cities, key=lambda x: x.name)
-        cities_dict[state.id] = sorted_cities
-    return render_template('8-cities_by_states.html', states=sorted_states,
-                           cities_dict=cities_dict)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
